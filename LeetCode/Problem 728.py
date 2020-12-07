6d0602f33f12eabb35c07e0f5d803cfcbc0dbaa3
Ben Sparks <bensparks0@gmail.com>
2020-12-07 11:19:19 +0200

    Create Problem 728.py

---
+++ LeetCode/Problem 728.py
@@ -0,0 +1,22 @@
+# First attempt
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        res = []
+        for i in range(left, right+1):
+            numStrList = list(str(i))
+            if('0' in numStrList): continue
+            isSelfDivNum = str(i).replace("0","") == "".join([j for j in numStrList if i % int(j) == 0])
+            if(isSelfDivNum): res.append(i)
+        return res
+
+# Second attempt - learnt For/Else in Python
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        res = []
+        for i in range(left, right+1):
+            for char in str(i):
+                if(char == '0' or i % int(char) != 0): break
+            else:
+                res.append(i)
+        return res
+    
