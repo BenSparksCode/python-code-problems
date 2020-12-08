a45624b831f3933b14ff52ef388c0a153e776648
Ben Sparks <bensparks0@gmail.com>
2020-12-09 00:08:51 +0200

    Problem 922 improvements

--- LeetCode/Problem 922.py
+++ LeetCode/Problem 922.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/sort-array-by-parity-ii
 
+# Attempt 1 - filter into sub arrays, then rebuild in order - slower, less memory
 class Solution:
     def sortArrayByParityII(self, A: List[int]) -> List[int]:
         odds = [i for i in A if i%2==1]
@@ -8,4 +9,20 @@ class Solution:
         for i in range(0,len(odds)):
             result.append(evens[i])
             result.append(odds[i])
+        return result
+
+# Attempt 2 - 2 pass - faster, more memory
+class Solution:
+    def sortArrayByParityII(self, A: List[int]) -> List[int]:
+        result = [None] * len(A)
+        pntr = 0 #evens first
+        for val in A:
+            if(val%2==0):
+                result[pntr] = val
+                pntr += 2
+        pntr = 1 #odds next
+        for val in A:
+            if(val%2==1):
+                result[pntr] = val
+                pntr += 2
         return result
