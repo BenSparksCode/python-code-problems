80103f3b256701e6ceceef8f14ea60a26021ba67
Ben Sparks <bensparks0@gmail.com>
2020-12-10 11:04:03 +0200

    Problem 136

---
+++ LeetCode/Easy/Problem 136.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/single-number/
+
+# Attempt 1 - Using dict for count - O(n) but needs extra memory
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        cnts = {}
+        for i in nums:
+            if (i in cnts):
+                cnts[i] += 1
+            else: #can use defaultdict to default each value to 0 (int default)
+                cnts[i] = 1
+        for key in cnts:
+            if (cnts[key]==1): return key
+        return
+
