77d29a592e874a6684ba2627a81f04a8ac9ab805
Ben Sparks <bensparks0@gmail.com>
2020-12-16 23:49:54 +0200

    Improved Problem 486
    Added memoization to the original recursive minimax solution - results in 160x improvement in runtime speed

--- LeetCode/Medium/Problem 486.py
+++ LeetCode/Medium/Problem 486.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/predict-the-winner/
 
 # ATTEMPT 1 - Recursive Minimax, no memo
+# TIME: O(2^n)
+# SPACE: O(n)
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         def winner(nums, s, e, turn):
@@ -9,3 +11,18 @@ class Solution:
             b = turn * nums[e] + winner(nums, s, e-1, -turn)
             return max(a,b) if turn==1 else min(a,b) 
         return winner(nums, 0, len(nums)-1, 1) >= 0
+
+# ATTEMPT 2 - Recursive Minimax, with memo
+# TIME: O(n^2) - nxn memo dict filled only once
+# SPACE: O(n^2) - nxn dictionary
+# 160x improvement in runtime speed on LeetCode over ATTEMPT 1
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        def winner(s,e):
+            if s == e: return nums[s]
+            if (s,e) in memo: return memo[(s,e)]
+            score = max(nums[e]-winner(s,e-1), nums[s] - winner(s+1, e))
+            memo[(s,e)] = score
+            return score
+        memo = {}
+        return winner(0, len(nums)-1) >= 0
