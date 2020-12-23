685677f00731dcc1137edbd984f11e9045f81f8e
Ben Sparks <bensparks0@gmail.com>
2020-12-23 23:59:24 +0200

    Finished Problem 322

--- LeetCode/Medium/Problem 322.py
+++ LeetCode/Medium/Problem 322.py
@@ -1,25 +1,16 @@
 # https://leetcode.com/problems/coin-change/
 
-# ATTEMPT 1 - Backtracking by Brute Force
+# ATTEMPT 1 - Backtracking with dynamic programming
 # TIME = 
 # SPACE = 
 
-# TODO
-
-
-
-# ATTEMP 2 - Backtracking faster with top down dynamic programming
-# TIME = 
-# SPACE = 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        def helper(coins, rem, count):
-            if rem < 0: return -1
-            if rem == 0: return 0
-            if count[rem-1] != 0: return count[rem-1]
-            min = float('inf')
+        dp = [0] + [float('inf') for i in range(amount)]
+        for i in range(1, amount+1):
             for coin in coins:
-                res = helper(coins, rem-coin, count)
-                if res >= 0 and res < min: min = 1 + res
-            count[rem-1] = -1 if min == float('inf') else min
-            return count[rem - 1]
+                if i - coin >= 0:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        if dp[-1] == float('inf'):
+            return -1
+        return dp[-1]
