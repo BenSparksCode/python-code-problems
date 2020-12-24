f12834d9083ce733ce5b3b35efdb35af2940adc2
Ben Sparks <bensparks0@gmail.com>
2020-12-24 12:33:18 +0200

    Rewrote solution to Problem 322

--- LeetCode/Medium/Problem 322.py
+++ LeetCode/Medium/Problem 322.py
@@ -1,16 +1,15 @@
 # https://leetcode.com/problems/coin-change/
 
 # ATTEMPT 1 - Backtracking with dynamic programming
-# TIME = 
-# SPACE = 
+# TIME = O(n*m) where n = amount, m = length of coins
+# SPACE = O(n) where n = amount
 
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [0] + [float('inf') for i in range(amount)]
-        for i in range(1, amount+1):
+    def coinChange(self, coins: 'List[int]', amount: 'int') -> 'int':
+        dp = [0]+[float('inf')]*amount
+        for i in range(1, amount + 1):
             for coin in coins:
                 if i - coin >= 0:
-                    dp[i] = min(dp[i], dp[i-coin] + 1)
-        if dp[-1] == float('inf'):
-            return -1
+                    dp[i] = min(dp[i], dp[i-coin]+1)
+        if dp[-1] == float('inf'): return -1
         return dp[-1]
