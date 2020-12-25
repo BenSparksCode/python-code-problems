f4878f2311ac83241a34d9251c215d122647ed4c
Ben Sparks <bensparks0@gmail.com>
2020-12-25 23:32:09 +0200

    Progress on Problem 15

--- LeetCode/Medium/Problem 15.py
+++ LeetCode/Medium/Problem 15.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/3sum/
 
 # ATTEMPT 1 - 2 pointers simultaneously closing in on 0
+# RESULT = Wrong answer when nums is symmetrical e.g. [-2,-1,0,1,2]
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -35,7 +36,8 @@ class Solution:
         return list(res)
 
 
-# ATTEMPT 2 - 3 pointers (anchor, p1, p2). Anchor moves in from each side. p1 and p2 pinned
+# ATTEMPT 2 - 3 pointers (anchor, p1, p2), anchor as all negatives then all positives.
+# RESULT = Wrong answer on later tests 
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -49,26 +51,87 @@ class Solution:
         
         # Anchor on left
         while anchor < nums_len and sort_nums[anchor] <= 0:
-            while sort_nums[p2] >= 0 and p2 > anchor:
+            while sort_nums[p1] >= 0 and sort_nums[p2] >= 0 and p2 > anchor:
                 if sort_nums[anchor] + sort_nums[p2] + sort_nums[p1] == 0:
                     res.add((sort_nums[anchor], sort_nums[p2], sort_nums[p1]))
-                p1 -= 1
-                p2 -= 1
-            
+                if sort_nums[anchor] + sort_nums[p1] < sort_nums[p2] and sort_nums[p2] > 0:
+                    p2 -= 1
+                else:
+                    p1 -= 1
+                    p2 = p1 - 1
+                    
             anchor += 1
             p1, p2 = nums_len-1, nums_len-2
             
         # Anchor on right
         anchor, p1, p2 = nums_len-1, 0, 1
         while anchor > 0 and sort_nums[anchor] > 0:
-            while sort_nums[p2] <= 0 and p2 < anchor:
+            while sort_nums[p1] <= 0 and sort_nums[p2] <= 0 and p2 < anchor:
                 if sort_nums[anchor] + sort_nums[p2] + sort_nums[p1] == 0:
                     res.add((sort_nums[p1], sort_nums[p2], sort_nums[anchor]))
-                p1 += 1
-                p2 += 1
+                if sort_nums[anchor] + sort_nums[p1] > sort_nums[p2] and sort_nums[p2] < 0:
+                    p2 += 1
+                else:
+                    p1 += 1
+                    p2 = p1 + 1
             
             anchor -= 1
             p1, p2 = 0, 1
             
-        return list(res) 
+        return list(res)
+
+# ATTEMPT 3 - Removed p2 pointer - calc it instead with anchor - p1
+# RESULT = Seems to pass all tests but hits TIME LIMIT EXCEEDED
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums_len = len(nums)
+        if nums_len < 3: return []
+        sort_nums = sorted(nums)
+        if sort_nums[0] > 0 or sort_nums[-1] < 0: return []
+        
+        anchor, p1 = 0, nums_len-1
+        res = set()
+        
+        # Anchor on left
+        while anchor < nums_len and sort_nums[anchor] <= 0:
+            while sort_nums[p1] >= 0 and p1 > anchor:
+                if sort_nums[p1] < -0.5*sort_nums[anchor]: break
+                
+                third_num = -(sort_nums[anchor] + sort_nums[p1])
+                if third_num >= 0 and third_num <= sort_nums[p1] and third_num in sort_nums:
+                    if third_num != sort_nums[p1]:
+                        res.add((sort_nums[anchor], third_num, sort_nums[p1]))
+                    else:
+                        if third_num != sort_nums[anchor] and third_num == sort_nums[p1] and sort_nums.count(third_num) > 1:
+                            res.add((sort_nums[anchor], third_num, sort_nums[p1]))
+                        elif third_num == sort_nums[anchor] and third_num == sort_nums[p1] and sort_nums.count(third_num) > 2:
+                            res.add((sort_nums[anchor], third_num, sort_nums[p1]))                        
+                p1 -= 1
+                    
+            anchor += 1
+            p1 = nums_len-1
+            
+        # Anchor on right
+        anchor, p1 = nums_len-1, 0
+        while anchor > 0 and sort_nums[anchor] > 0:
+            while sort_nums[p1] <= 0 and p1 < anchor:
+                if sort_nums[p1] > -0.5*sort_nums[anchor]: break
+                
+                third_num = -(sort_nums[anchor] + sort_nums[p1])
+                if third_num < 0 and third_num >= sort_nums[p1] and third_num in sort_nums:
+                    if third_num != sort_nums[p1]:
+                        res.add((sort_nums[p1], third_num, sort_nums[anchor]))
+                    else:
+                        if third_num == sort_nums[p1] and sort_nums.count(third_num) > 1:
+                            res.add((sort_nums[p1], third_num, sort_nums[anchor]))
+                        
+                p1 += 1
+                    
+            anchor -= 1
+            p1 = 0
+            
+        return list(res)
+
+
 
