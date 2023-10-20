7caa55ae0cf886d8ba18b91f7d5a48749a29a194
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-10-20 20:57:34 +0200

    Start PE Problem 22

---
+++ Project Euler/problem22.py
@@ -0,0 +1,18 @@
+# https://projecteuler.net/problem=22
+# Using names.txt, work out the alphabetical value for each name,
+# multiply this value by its alphabetical position in
+# the list to obtain a name score.
+# What is the total of all the name scores in the file?
+
+import time
+start_time = time.time()
+
+
+
+# TODO
+
+
+
+
+print("SOLUTION:", solution)
+print("--- %s seconds ---" % (time.time() - start_time))
