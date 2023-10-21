95b544615b5204ce808192619d86f9e6751b2917
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-10-21 11:35:43 +0200

    Solved PE Problem 22

--- Project Euler/problem22.py
+++ Project Euler/problem22.py
@@ -4,24 +4,28 @@
 # the list to obtain a name score.
 # What is the total of all the name scores in the file?
 
+import os
 import time
 start_time = time.time()
 
-namesFileDir = "./assets/names.txt"
+namesFileDir = '/Project Euler/assets/problem22 - names.txt'
 totalNameScores = 0
 nameList = []
 
-def loadNameListAndSort(dir: string) -> list:
+def loadNameListAndSort(dir: str) -> list:
+    rawTextString = ""
+    with open(os.getcwd() + dir) as f:
+        rawTextString = f.readline()
+    return sorted(rawTextString.replace('"',"").split(","))
 
-    return []
-
-def calculateLetterScore(word: string) -> int:
-
-    return 0
+def calculateLetterScore(word: str) -> int:
+    return sum([ord(c) - 64 for c in word])
 
 def calculateSumOfNameScores(names: list) -> int:
-
-    return 0
+    sumScores = 0
+    for i, name in enumerate(names, start = 1):
+        sumScores += i * calculateLetterScore(name)
+    return sumScores
 
 nameList = loadNameListAndSort(namesFileDir)
 totalNameScores = calculateSumOfNameScores(nameList)
