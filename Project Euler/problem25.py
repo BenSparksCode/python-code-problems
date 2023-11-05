9d5dd1e295b5fc578a9faadd32472418a3747599
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-05 11:04:36 +0200

    Start PE Problem 25

---
+++ Project Euler/problem25.py
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=25
+# What is the index of the first term in the Fibonacci
+# sequence to contain 1000 digits?
+
+import time
+start_time = time.time()
+
+# SOLUTION CODE STARTS HERE
+
+
+
+# SOLUTION CODE ENDS HERE
+
+solution = 0 # TODO 
+print("SOLUTION:", solution)
+print("--- %s seconds ---" % (time.time() - start_time))
