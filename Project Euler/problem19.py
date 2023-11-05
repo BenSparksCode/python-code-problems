552bc27a3ae2107f6a3f6d8da57e2dbb941a6b99
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-05 11:01:06 +0200

    Start PE Problem 19

---
+++ Project Euler/problem19.py
@@ -0,0 +1,16 @@
+# https://projecteuler.net/problem=19
+# How many Sundays fell on the first of the month
+# during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+import time
+start_time = time.time()
+
+# SOLUTION CODE STARTS HERE
+
+
+
+# SOLUTION CODE ENDS HERE
+
+solution = 0 # TODO 
+print("SOLUTION:", solution)
+print("--- %s seconds ---" % (time.time() - start_time))
