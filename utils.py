aa0b3fe8979ed9cc5e417f824a2bd343aa2f5897
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2023-11-11 21:22:43 +0200

    Improve timer decorator

--- utils.py
+++ utils.py
@@ -3,13 +3,17 @@ import time
 
 def timer(func: Callable) -> Callable:
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time = time.perf_counter()
         result = func(*args, **kwargs)
-        end_time = time.time()
+        end_time = time.perf_counter()
+
         print()
-        print("----------------  RUN TIME  ---------------")
-        print("-\t%s seconds\t  -" % (end_time - start_time))
-        print("-------------------------------------------")
+        print("-------------------------------------------------------------")
+        print(f"|\tFunction {func.__name__}() executed in:")
+        print("|")
+        print(f"|\t\t\t{end_time - start_time:.8f} seconds")
+        print("-------------------------------------------------------------")
         print()
+
         return result
     return wrapper
